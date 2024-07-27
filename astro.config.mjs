a524150d173439e227b13670e6b303fb9740d913
Paul Chavez <paulchavezromero20@gmail.com>
2024-07-27 00:01:02 -0500

    feat: getting-started added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,29 +6,31 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: "Custom Hooks",
-    social: {
-      github: "https://github.com/developaul/custom-hooks"
-    },
-    sidebar: [{
-      label: "Guides",
-      autogenerate: {
-        directory: "guides"
-      }
-    }, {
-      label: "Hooks",
-      autogenerate: {
-        directory: "hooks"
-      }
-    }, {
-      label: "Reference",
-      autogenerate: {
-        directory: "reference"
-      }
-    }],
-    customCss: ["./src/tailwind.css"]
-  }), tailwind({
-    applyBaseStyles: false
-  }), react()]
+  integrations: [
+    starlight({
+      title: "Custom Hooks",
+      social: {
+        github: "https://github.com/developaul/custom-hooks",
+      },
+      sidebar: [
+        {
+          label: "Getting Started",
+          autogenerate: {
+            directory: "getting-started",
+          },
+        },
+        {
+          label: "Hooks",
+          autogenerate: {
+            directory: "hooks",
+          },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
